076e7c90721943ea30dc0e8e17d11f8e3720720f
MarcoCotas <marco.cotas06@gmail.com>
2025-03-27 19:17:01 +0000

    Finish random rgb on hoover function

--- dom.js
+++ dom.js
@@ -5,22 +5,46 @@ In css edit the block divs to have a grid
 */
 
 const container = document.querySelector("#container");
-const gridSize = 16;
+const gridSize = parseInt(prompt("Whats your number"));
 const containerWidth = 500;
 const divSize = containerWidth / gridSize;
 
 function grid(num) {
   let totalgrids = num * num;
   for (let i = 0; i < totalgrids; i++) {
-    const myDiv = document.createElement("div");
-    myDiv.style.width = `${divSize}px`;
-    myDiv.style.height = `${divSize}px`;
-    myDiv.style.border = "1px solid black";
-    myDiv.style.backgroundColor = "lightblue";
-    myDiv.style.boxSizing = "border-box";
+    const myBtn = document.createElement("button");
+    myBtn.style.width = `${divSize}px`;
+    myBtn.style.height = `${divSize}px`;
+    myBtn.style.border = "1px solid black";
+    myBtn.style.backgroundColor = "lightblue";
+    myBtn.style.boxSizing = "border-box";
 
-    myDiv.classList.add("griditem");
-    container.appendChild(myDiv);
+    myBtn.classList.add("grid-item");
+    container.appendChild(myBtn);
   }
 }
 grid(gridSize);
+
+document.body.addEventListener("mouseover", (e) => {
+  let button = e.target.closest("button");
+  if (!button) {
+    return;
+  }
+  button.style.backgroundColor = rgb();
+});
+
+document.body.addEventListener("mouseout", (e) => {
+  let button = e.target.closest("button");
+  if (!button) {
+    return;
+  }
+  button.style.backgroundColor = rgb();
+});
+
+function rgb() {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+  return rgb;
+}
