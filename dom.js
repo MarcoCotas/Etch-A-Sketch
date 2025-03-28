3721803535159799c87cc5be9ffef1faadf7e69c
Marco Cotas <marco.cotas06@gmail.com>
2025-03-28 19:22:54 +0000

    Update the images url from / to ./ to fix github img laoding

--- dom.js
+++ dom.js
@@ -84,15 +84,15 @@ document.querySelectorAll(".header button").forEach((button) => {
         break;
       case "Dark mode":
         document.body.style.backgroundColor = "black";
-        document.body.style.backgroundImage = 'url("images/party.jpg")';
+        document.body.style.backgroundImage = 'url("./images/party.jpg")';
         event.target.textContent = "Background";
         title.style.color = "white";
 
         break;
       case "Background":
-        document.body.style.backgroundImage = 'url("images/retro.jpg")';
+        document.body.style.backgroundImage = 'url("./images/retro.jpg")';
         event.target.textContent = "Light mode";
-        title.style.color = "yellow";
+        title.style.textContent = "red";
 
         break;
     }
