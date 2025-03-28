2ea6d8980a3c3101290333495a3817c69bc7bd73
Marco Cotas <marco.cotas06@gmail.com>
2025-03-28 18:33:12 +0000

    Finish the final 1st version of the project

--- dom.js
+++ dom.js
@@ -2,6 +2,7 @@ const container = document.querySelector("#container");
 let gridSize = 16;
 const containerWidth = 500;
 const divSize = containerWidth / gridSize;
+const title = document.querySelector(".title");
 
 function getGrid() {
   gridSize = parseInt(prompt("What´s the size of your grid"));
@@ -70,7 +71,7 @@ document.querySelectorAll(".header button").forEach((button) => {
 
       case "Erase":
         document.querySelectorAll(".gridButton").forEach((cell) => {
-          cell.style.backgroundColor = "white"; // Ou a cor inicial desejada
+          cell.style.backgroundColor = "white";
         });
         break;
 
@@ -78,15 +79,21 @@ document.querySelectorAll(".header button").forEach((button) => {
         document.body.style.backgroundImage = "none";
         document.body.style.backgroundColor = "white";
         event.target.textContent = "Dark mode";
+        title.style.color = "black";
+
         break;
       case "Dark mode":
-        document.body.style.backgroundImage = "none";
         document.body.style.backgroundColor = "black";
+        document.body.style.backgroundImage = 'url("/images/party.jpg")';
         event.target.textContent = "Background";
+        title.style.color = "white";
+
         break;
       case "Background":
         document.body.style.backgroundImage = 'url("/images/retro.jpg")';
         event.target.textContent = "Light mode";
+        title.style.color = "yellow";
+
         break;
     }
   });
